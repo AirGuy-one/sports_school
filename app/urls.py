070e21b64fe441312938dadf33b54c735359b684
AirGuy-one <airguy-one@yandex.ru>
2023-05-02 12:04:53 +0300

    Add html page stubs for each page in the "about organization" list

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,15 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('basic_info', views.basic_info, name='basic_info_url'),
+    path('structure_management', views.structure_management, name='structure_management_url'),
+    path('available_environment', views.available_environment, name='available_environment_url'),
+    path('documents', views.documents, name='documents_url'),
+    path('education', views.education, name='education_url'),
+    path('financial_and_economic', views.financial_and_economic, name='financial_and_economic_url'),
+    path('international_cooperation', views.international_cooperation, name='international_cooperation_url'),
+    path('paid_services', views.paid_services, name='paid_services_url'),
+    path('pedagogical_staff', views.pedagogical_staff, name='pedagogical_staff_url'),
+    path('support_and_equipment', views.support_and_equipment, name='support_and_equipment_url'),
+    path('vacant_seats', views.vacant_seats, name='vacant_seats_url'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,10 +6,52 @@ def index(request):
     news = News.objects.all().order_by('-id').prefetch_related('photos')
     games = Games.objects.all()
 
-    print(games)
-
     context = {
         'news': news,
     }
 
     return render(request, 'index.html', context)
+
+
+def basic_info(request):
+    return render(request, 'basic_information.html')
+
+
+def structure_management(request):
+    return render(request, 'structure_and_management.html')
+
+
+def available_environment(request):
+    return render(request, 'available_environment.html')
+
+
+def documents(request):
+    return render(request, 'documents.html')
+
+
+def education(request):
+    return render(request, 'education.html')
+
+
+def financial_and_economic(request):
+    return render(request, 'financial_and_economic.html')
+
+
+def international_cooperation(request):
+    return render(request, 'international_cooperation.html')
+
+
+def paid_services(request):
+    return render(request, 'paid_services.html')
+
+
+def pedagogical_staff(request):
+    return render(request, 'pedagogical_staff.html')
+
+
+def support_and_equipment(request):
+    return render(request, 'support_and_equipment.html')
+
+
+def vacant_seats(request):
+    return render(request, 'vacant_seats.html')
